39ac2a05391cc58f2c159196afdcbedafbeea344
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:22:00 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,8 +1,20 @@
+export declare const b__margin__svg: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __margin__svg: import("svelte/store").Writable<any>;
+export declare const b__width__svg: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __width__svg: import("svelte/store").Writable<any>;
+export declare const b__height__svg: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __height__svg: import("svelte/store").Writable<any>;
+export declare const b__width__content__svg: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __width__content__svg: import("svelte/store").Writable<any>;
+export declare const b__height__content__svg: (ctx?: any, opts?: any) => import("svelte/store").Writable<any>;
 export declare const __height__content__svg: import("svelte/store").Writable<any>;
+export declare const b__ctx__matrix2d__svg: (ctx?: any, opts?: any) => import("svelte/store").Readable<{
+    margin__svg: any;
+    width__svg: any;
+    height__svg: any;
+    width__content__svg: any;
+    height__content__svg: any;
+}>;
 export declare const __ctx__matrix2d__svg: import("svelte/store").Readable<{
     margin__svg: any;
     width__svg: any;
--- store.ts
+++ store.ts
@@ -1,26 +1,39 @@
 import { writable, derived } from 'svelte/store'
-export const __margin__svg = writable(null)
-export const __width__svg = writable(null)
-export const __height__svg = writable(null)
-export const __width__content__svg = writable(null)
-export const __height__content__svg = writable(null)
-export const __ctx__matrix2d__svg = derived([
-		__margin__svg,
-		__width__svg,
-		__height__svg,
-		__width__content__svg,
-		__height__content__svg,
-	],
-	([
-		 margin__svg,
-		 width__svg,
-		 height__svg,
-		 width__content__svg,
-		 height__content__svg
-	 ]) => ({
-		margin__svg,
-		width__svg,
-		height__svg,
-		width__content__svg,
-		height__content__svg,
-	}))
+import { _b } from '@ctx-core/object'
+export const b__margin__svg = _b('__margin__svg', ()=>
+	writable(null))
+export const __margin__svg = b__margin__svg()
+export const b__width__svg = _b('__width__svg', ()=>
+	writable(null))
+export const __width__svg = b__width__svg()
+export const b__height__svg = _b('__height__svg', ()=>
+	writable(null))
+export const __height__svg = b__height__svg()
+export const b__width__content__svg = _b('__width__content__svg', ()=>
+	writable(null))
+export const __width__content__svg = b__width__content__svg()
+export const b__height__content__svg = _b('__height__content__svg', ()=>
+	writable(null))
+export const __height__content__svg = b__height__content__svg()
+export const b__ctx__matrix2d__svg = _b('__ctx__matrix2d__svg', ctx=>
+	derived([
+			b__margin__svg(ctx),
+			b__width__svg(ctx),
+			b__height__svg(ctx),
+			b__width__content__svg(ctx),
+			b__height__content__svg(ctx),
+		],
+		([
+			 margin__svg,
+			 width__svg,
+			 height__svg,
+			 width__content__svg,
+			 height__content__svg
+		 ])=>({
+			margin__svg,
+			width__svg,
+			height__svg,
+			width__content__svg,
+			height__content__svg,
+		})))
+export const __ctx__matrix2d__svg = b__ctx__matrix2d__svg()
