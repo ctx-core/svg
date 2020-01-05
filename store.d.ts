0860f656de493c48d56e203550cf42243a8697fa
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:04:28 -0500

    feat: store: b__ factories
    BREAKING CHANGE: set__matrix2d__svg moved into store

--- store.d.ts
+++ store.d.ts
@@ -22,3 +22,16 @@ export declare const __ctx__matrix2d__svg: import("svelte/store").Readable<{
     width__content__svg: any;
     height__content__svg: any;
 }>;
+export declare type Placement = {
+    top?: number;
+    right?: number;
+    bottom?: number;
+    left?: number;
+};
+export declare type Opts__set__matrix2d__svg = {
+    margin__svg?: Placement;
+    width__svg?: number;
+    height__svg?: number;
+};
+export declare const b__set__matrix2d__svg: (ctx?: any, opts?: any) => (opts?: Opts__set__matrix2d__svg) => Promise<void>;
+export declare const set__matrix2d__svg: (opts?: Opts__set__matrix2d__svg) => Promise<void>;
--- store.ts
+++ store.ts
@@ -1,5 +1,7 @@
-import { writable, derived } from 'svelte/store'
+import { writable, derived, get } from 'svelte/store'
 import { _b } from '@ctx-core/object'
+import { log } from '@ctx-core/logger'
+const logPrefix = '@ctx-core/svg/store.js'
 export const b__margin__svg = _b('__margin__svg', ()=>
 	writable(null))
 export const __margin__svg = b__margin__svg()
@@ -37,3 +39,46 @@ export const b__ctx__matrix2d__svg = _b('__ctx__matrix2d__svg', ctx=>
 			height__content__svg,
 		})))
 export const __ctx__matrix2d__svg = b__ctx__matrix2d__svg()
+export type Placement = {
+	top?:number
+	right?:number
+	bottom?:number
+	left?:number
+}
+export type Opts__set__matrix2d__svg = {
+	margin__svg?:Placement
+	width__svg?:number
+	height__svg?:number
+}
+export const b__set__matrix2d__svg = _b('set__matrix2d__svg', ctx=>{
+	return set__matrix2d__svg
+	async function set__matrix2d__svg(
+		opts:Opts__set__matrix2d__svg = {}
+	) {
+		log(`${logPrefix}|set__matrix2d__svg`)
+		const margin__svg =
+			opts.margin__svg
+			|| get(b__margin__svg(ctx))
+			|| { top: 20, right: 20, bottom: 60, left: 100 }
+		const width__svg =
+			opts.width__svg
+			|| get(b__width__svg(ctx))
+		const height__svg =
+			opts.height__svg
+			|| get(b__height__svg(ctx))
+		const {
+			left,
+			right,
+			top,
+			bottom,
+		} = margin__svg
+		const width__content__svg = width__svg - left - right
+		const height__content__svg = height__svg - top - bottom
+		b__margin__svg(ctx).set(margin__svg)
+		b__width__svg(ctx).set(width__svg)
+		b__height__svg(ctx).set(height__svg)
+		b__width__content__svg(ctx).set(width__content__svg)
+		b__height__content__svg(ctx).set(height__content__svg)
+	}
+})
+export const set__matrix2d__svg = b__set__matrix2d__svg()
