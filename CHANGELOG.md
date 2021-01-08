f62d0f8b634120cccb260c3d2cfc44661d1398a9
Brian Takita <brian.takita@gmail.com>
2021-01-08 00:39:09 -0500

    ## 9.0.24
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - Updated dependencies [undefined]
    - @ctx-core/sapper@9.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # @ctx-core/svg
 
+## 9.0.24
+
+### Patch Changes
+
+- Updated dependencies [undefined]
+- Updated dependencies [undefined]
+  - @ctx-core/sapper@9.0.0
+
 ## 9.0.23
 
 ### Patch Changes
--- [icon_name].svg.ts
+++ [icon_name].svg.ts
@@ -7,16 +7,8 @@ import { getOuterHTML } from 'domutils'
 import { throw_not_found } from '@ctx-core/error'
 import type { Element } from 'domhandler'
 import type { Request, Response } from 'express'
-import type { params_type, query_type } from '@ctx-core/sapper'
 import resolve from 'resolve'
 const resolve_promise = promisify(resolve)
-export interface _get_opts_type_fn_req_type {
-	fn(req:Request, res:Response):Promise<void>
-}
-export interface _get_opts_type {
-	fn?:(req:Request, res:Response)=>Promise<any>
-	resolve?:(path:string)=>string
-}
 /**
  * Returns a `get` http handler that processes the svelte component whose path
  * is returned from `opts.resolve`.
@@ -27,8 +19,8 @@ export function _get(opts = {} as _get_opts_type) {
 	if (typeof resolve !== 'function') throw 'opts.resolve must be a function'
 	return async function get(req:Request, res:Response) {
 		res.setHeader('Content-Type', 'image/svg+xml')
-		const { icon_name } = req.params as params_type
-		const { style } = req.query as query_type
+		const { icon_name } = req.params
+		const { style } = req.query
 		if (fn) await fn(req, res)
 		let svg = ''
 		const handler = new DomHandler((error, dom)=>{
@@ -59,3 +51,10 @@ export function _get(opts = {} as _get_opts_type) {
 }
 export type _get_type = (opts?:_get_opts_type)=>get_type
 export type get_type = (req:Request, res:Response)=>Promise<void>
+export interface _get_opts_type_fn_req_type {
+	fn(req:Request, res:Response):Promise<void>
+}
+export interface _get_opts_type {
+	fn?:(req:Request, res:Response)=>Promise<any>
+	resolve?:(path:string)=>string
+}
